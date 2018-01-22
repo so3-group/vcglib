60038dfb12de3d9db818472206135d95eef40336
haldean <me@haldean.org>
2018-01-22 13:23:14 -0800

    add ability to turn off assertions for testing

--- vcg/math/assert.h
+++ vcg/math/assert.h
@@ -9,4 +9,8 @@ struct assertion_failed : std::runtime_error {
 };
 };
 
+#ifndef VCG_ASSERT_DOES_NOTHING
 #define vcg_assert(x) do { if (!(x)) throw assertion_failed("assertion failed: " #x); } while(0)
+#else
+#define vcg_assert(x) do {} while (0)
+#endif
